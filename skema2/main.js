b301c1d34c39b69a86bb67da491b94b2be7c7de2
limmi26 <kisamakiazuel@gmail.com>
2023-03-27 14:47:40 +0700

    perubahan 3

--- skema2/main.js
+++ skema2/main.js
@@ -1,32 +1,110 @@
-// list harga hotel
-const data = [
-    {sewa: '1-2 hari', superior: '100000/malam', deluxe: '150000/malam', premium: '200000/malam'},
-    {sewa: '3-4 hari', superior: '90000/malam', deluxe: '135000/malam', premium: '180000/malam'},
-    {sewa: '>=5 hari', superior: '80000/malam', deluxe: '120000/malam', premium: '160000/malam'}
+// import readline
+const readline = require("readline");
+
+// buatlah daftar harga
+const list = [
+  {
+    type: "Superior",
+    "1-2 hari": 100000,
+    "3-4 hari": 90000,
+    ">=5 hari": 80000,
+  },
+  {
+    type: "Deluxe",
+    "1-2 hari": 150000,
+    "3-4 hari": 135000,
+    ">=5 hari": 120000,
+  },
+  {
+    type: "Premium",
+    "1-2 hari": 200000,
+    "3-4 hari": 180000,
+    ">=5 hari": 160000,
+  },
 ];
 
-// input dan output data
-console.table(data);
+console.table(list, ["type", "1-2 hari", "3-4 hari", ">=5 hari"], {
+  showIndex: false,
+});
+console.log("Keterangan :");
+console.log("1. Superior");
+console.log("2. Deluxe");
+console.log("3. Premium");
 
-const readline = require("readline");
+// tentukan harga
+const harga = {
+  1: [
+    { hari: 1, harga: 100000 },
+    { hari: 2, harga: 100000 },
+    { hari: 3, harga: 90000 },
+    { hari: 4, harga: 90000 },
+    { hari: 5, harga: 80000 },
+    { hari: Infinity, harga: 80000 },
+  ],
+  2: [
+    { hari: 1, harga: 150000 },
+    { hari: 2, harga: 150000 },
+    { hari: 3, harga: 135000 },
+    { hari: 4, harga: 135000 },
+    { hari: 5, harga: 120000 },
+    { hari: Infinity, harga: 120000 },
+  ],
+  3: [
+    { hari: 1, harga: 200000 },
+    { hari: 2, harga: 200000 },
+    { hari: 3, harga: 180000 },
+    { hari: 4, harga: 180000 },
+    { hari: 5, harga: 160000 },
+    { hari: Infinity, harga: 160000 },
+  ],
+};
+
+// tipe menjadi nama
+const jenisKamar = {
+  1: "Superior",
+  2: "Deluxe",
+  3: "Premium",
+};
+
+// process input
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-// memesan hotel
-rl.question("Tipe kamar: \n 1. superior \n 2. deluxe \n 3. premium \n pilih tipe kamar: ", (kamar) => {
-    rl.question("lama menginap: ", (inap) => {
-        let harga = 0;
-        if(kamar === "1"){
-            harga = inap <= 2 ? 100000 : inap <= 4 ? 90000 : 80000;
-        } else if(kamar === "2"){
-            harga = inap <= 2 ? 150000 : inap <= 4 ? 135000 : 1200000;
-        } else if(kamar){
-            harga = inap <= 2 ? 200000 : inap <= 4 ? 180000 : 160000;
-        }
+function booking() {
+  rl.question(
+    "Pilih tipe kamar : ",
+    (tipeKamar) => {
+      if (tipeKamar in jenisKamar) {
+        rl.question(
+          "Lama menginap : ",
+          (lama) => {
+            const tipe = jenisKamar[tipeKamar];
+            const hargaKamar = harga[tipeKamar];
+            const hargaMalam = hargaKamar.find(
+              (data) => lama <= data.hari
+            );
+            const total = hargaMalam.harga * lama;
+            console.log(`Total Harga: Rp ${total}`); //total harga
 
-        const total = harga * inap;
-        console.log("total harga: " + total);
-    });
-});
+            // askAgain();
+            rl.question("Ingin memesan lagi? (y/n)", (answer) => {
+              if (answer === "y") {
+                booking();
+              } else {
+                console.log("terima kasih");
+                rl.close();
+              }
+            });
+          }
+        );
+      } else {
+        console.log("pilihan kamar tidak tersedia");
+        booking();
+      }
+    }
+  );
+}
+
+booking();
